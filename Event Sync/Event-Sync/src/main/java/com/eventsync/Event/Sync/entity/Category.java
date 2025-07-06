0fc11e95377c96a5d7bf5e411b9306f4e78b920a
Manujayaugp <pasindumanujaya85@gmail.com>
2025-07-06 12:03:27 +0530

    Update Category.java

--- Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/entity/Category.java
+++ Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/entity/Category.java
@@ -3,6 +3,7 @@ package com.eventsync.event.sync.entity;
 import java.time.LocalDateTime;
 import java.util.List;
 
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -13,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
+
 
 @Data
 @Entity
