97a89df3d5d50ccd2489211e4f98e848ce4cb6b4
Sahan Cooray <153707982+SahanCooray@users.noreply.github.com>
2025-07-07 12:18:45 +0530

    Update CategoryService.java

--- Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/service/interf/CategoryService.java
+++ Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/service/interf/CategoryService.java
@@ -1,1 +1,14 @@
+package com.eventsync.event.sync.service.interf;
+
+import com.eventsync.event.sync.dto.CategoryDto;
+import com.eventsync.event.sync.dto.Response;
+
+public interface CategoryService {
+
+    Response createCategory(CategoryDto categoryRequest);
+    Response updateCategory(Long categoryId, CategoryDto categoryRequest);
+    Response getAllCategories();
+    Response getCategoryById(Long categoryId);
+    Response deleteCategory(Long categoryId);
+}
 
