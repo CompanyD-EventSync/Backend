ae39e221fa5be612253fe03e8ebdb3a233dfa5dd
Mathisha Buwaneka Francisku Arachchi <mathib0417@gmail.com>
2025-07-09 09:08:59 +0530

    Update UserRepo.java

--- Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/repository/UserRepo.java
+++ Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/repository/UserRepo.java
@@ -1,5 +1,14 @@
-package com.eventsync.Event.Sync.repository;
+package com.eventsync.event.sync.repository;
 
-public class UserRepo {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eventsync.event.sync.entity.User;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    
+    Optional<User> findByEmail(String email);
+
     
 }
