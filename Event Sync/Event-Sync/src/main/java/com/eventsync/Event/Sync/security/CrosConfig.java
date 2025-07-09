b48507b7780760c12d7983880f99920aec2403bd
Mathisha Buwaneka Francisku Arachchi <mathib0417@gmail.com>
2025-07-09 09:10:00 +0530

    Update CrosConfig.java

--- Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/security/CrosConfig.java
+++ Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/security/CrosConfig.java
@@ -1,5 +1,22 @@
-package com.eventsync.Event.Sync.security;
+package com.eventsync.event.sync.security;
 
-public class CrosConfig {
-    
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("*");
+            }
+        };
+    }
 }
